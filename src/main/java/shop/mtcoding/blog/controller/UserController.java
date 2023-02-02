237f39157ee4bd11addf8cc4801c5e3ca29e6314
unknown <deliah07@naver.com>
2023-02-02 16:42:25 +0900

    view 연결

---
+++ src/main/java/shop/mtcoding/blog/controller/UserController.java
@@ -0,0 +1,41 @@
+package shop.mtcoding.blog.controller;
+
+import javax.servlet.http.HttpSession;
+import javax.websocket.Session;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import shop.mtcoding.blog.model.UserRepository;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    HttpSession session;
+
+    @GetMapping("/logout")
+    public String logout() {
+        session.invalidate();
+        return "redirect:/";
+    }
+
+    @GetMapping("/loginForm")
+    public String loginForm() {
+        return "/user/loginForm";
+    }
+
+    @GetMapping("/joinForm")
+    public String joinForm() {
+        return "/user/joinForm";
+    }
+
+    @GetMapping("/user/updateForm")
+    public String updateForm() {
+        return "/user/updateForm";
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/layout/footer.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+
+ <div class="container my-3">
+
+    </div>
+
+    <div class="mt-4 p-5 bg-dark text-white rounded text-center" style="margin-bottom: 0">
+        <p>Created by MetaCoding</p>
+        <p>📞 010-2222-7777</p>
+        <p>🏴 부산 수영구 XX동</p>
+    </div>
+</body>
+
+</html>
